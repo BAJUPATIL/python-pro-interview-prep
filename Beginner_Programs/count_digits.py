a669bd95c0d521702b1f243eabadc7adbb32966d
Pravin Kadam <31037035+galaxydevelopers@users.noreply.github.com>
2025-01-02 23:55:27 +0530

    adding count digit program
    adding count digit program
    
    Co-Authored-By: Bajarang Patil <123238148+BAJUPATIL@users.noreply.github.com>

--- Beginner_Programs/count_digits.py
+++ Beginner_Programs/count_digits.py
@@ -1,3 +1,9 @@
 # Program to count the number of digits in a number.
 # The program counts how many digits the number has.
 # Example: 12345 has 5 digits.
+str= input("enter the string to calculate digit:  ")
+count=0
+for i in str:
+    if i.isdigit():
+        count+=1
+print(count)
